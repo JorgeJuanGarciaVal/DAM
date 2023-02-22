9bfabdc4cc4723eabbc6e7aedded4dd612760025
JorgeJuanGarciaVal <126067391+JorgeJuanGarciaVal@users.noreply.github.com>
2023-02-22 15:29:14 +0100

    Create ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,10 @@
+def nombre():
+  hombre = input("pedro","pablo")
+  
+  if hombre == "pedro":
+    print("ME GUSTA TU NOMBRE")
+  elif hombre == "pablo":
+    print("NO ME GUSTA TU NOMBRE")
+          
+          
+ nombre()         
